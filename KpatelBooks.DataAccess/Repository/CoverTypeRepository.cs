49f310260bcc2455f22777c9c7270052ec003a7e
W0828783 <W0828783@ADL-A1201B-20>
2023-11-27 21:57:59 -0500

    error

---
+++ KpatelBooks.DataAccess/Repository/CoverTypeRepository.cs
@@ -0,0 +1,28 @@
+﻿using KpatelsBooks.DataAccess.Repository.IRepository;
+using KpatelsBooks.Models;
+using kpatelsBookStore.DataAccess.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KpatelsBooks.DataAccess.Repository
+{
+    public class CoverTypeRepository : Repository<CoverType>, ICoverTypeRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public CoverTypeRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+        public void Update(CoverType coverType)
+        {
+            var objFromDb = _db.CoverTypes.FirstOrDefault(s => s.Id == coverType.Id);
+            if (objFromDb != null)
+            {
+                objFromDb.Name = coverType.Name;
+                _db.SaveChanges();
+            }
+        }
+    }
+}
